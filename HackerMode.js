175bb2b0fbd198facc793e60766b23ca6f86849e
Andres Ruiz-Velediaz <aruiz858@gmail.com>
2020-02-04 08:15:09 -0800

    Removed striped

--- HackerMode.js
+++ HackerMode.js
@@ -11,12 +11,14 @@ $(window).on('load', ()=>{
 	$('body').css('background', 'black');
 	$('body').css('color', 'yellow');
 	$('.table-striped').removeClass("table-striped");
+	console.log($('.reportrange_right'));
+	$('.reportrange_right').css('background', 'rgb(0,0,0)');
 	css[2].cssRules[2541].style.backgroundColor = "rgb(0,0,0)";
 	css[2].cssRules[538].style.backgroundColor = "rgb(0,0,0)";
 	css[2].cssRules[72].style.background = "black";
 	css[2].cssRules[72].style.backgroundColor = "black";
 	css[2].cssRules[541].style.backgroundColor = "rgb(0,0,0)";
-	css[90].cssRules[5].style.background = "black";
+	css[92].cssRules[5].style.background = "black"; //Modal Background
 	css[2].cssRules[512].style.backgroundColor = "rgb(0, 0, 0)";
 	css[2].cssRules[2399].style.backgroundColor = "rgb(0, 0, 0)";
 	css[2].cssRules[512].cssText = `.table-striped > tbody > tr:nth-of-type(2n+1) { "background-color" : "#000000"}`;
