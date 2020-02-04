25a9af3e1e36b7bbb360f59cf3958c9d003e41d2
Andres Ruiz-Velediaz <aruiz858@gmail.com>
2020-02-04 07:54:37 -0800

    Removed striped

--- HackerMode.js
+++ HackerMode.js
@@ -3,18 +3,21 @@
 // Rows Color: 
 $(window).on('load', ()=>{
 	var cssRuleCode = document.all ? 'rules' : 'cssRules'; //account for IE and FF
+	$('.nav_title').remove();
 	let css = document.styleSheets;
 	console.log("CSS");
 	//console.log(css[2].cssRules[512].style.backgroundColor);
 	console.log(css);
 	$('body').css('background', 'black');
 	$('body').css('color', 'yellow');
+	$('.table-striped').removeClass("table-striped");
 	css[2].cssRules[2541].style.backgroundColor = "rgb(0,0,0)";
+	css[2].cssRules[538].style.backgroundColor = "rgb(0,0,0)";
 	css[2].cssRules[72].style.background = "black";
 	css[2].cssRules[72].style.backgroundColor = "black";
 	css[2].cssRules[541].style.backgroundColor = "rgb(0,0,0)";
 	css[90].cssRules[5].style.background = "black";
 	css[2].cssRules[512].style.backgroundColor = "rgb(0, 0, 0)";
 	css[2].cssRules[2399].style.backgroundColor = "rgb(0, 0, 0)";
-	// css[2].cssRules[512].cssText = `.table-striped > tbody > tr:nth-of-type(2n+1) { "background-color" : "#000000"}`;
+	css[2].cssRules[512].cssText = `.table-striped > tbody > tr:nth-of-type(2n+1) { "background-color" : "#000000"}`;
 });
