5ce7774c3d0733b1dcd86cc801b704c7a056e5d0
Andres Ruiz-Velediaz <aruiz858@gmail.com>
2020-02-04 20:02:12 -0800

    Date range + Biller Notes are black

--- HackerMode.js
+++ HackerMode.js
@@ -12,17 +12,17 @@ $(window).on('load', ()=>{
 	$('body').css('background', 'black');
 	$('body').css('color', 'yellow');
 	$('.table-striped').removeClass("table-striped");
-	$('.reportrange_right').css('background', 'rgb(0,0,0)');
+	$('#reportrange_right').css('background', 'rgb(0,0,0)');
 	css[2].cssRules[2541].style.backgroundColor = "rgb(0,0,0)";
 	css[2].cssRules[2357].style.backgroundColor = "rgb(0,0,0)";
 	css[2].cssRules[538].style.backgroundColor = "rgb(0,0,0)";
 	css[2].cssRules[72].style.background = "black";
 	css[2].cssRules[72].style.backgroundColor = "black";
 	css[2].cssRules[541].style.backgroundColor = "rgb(0,0,0)";
-	css[92].cssRules[5].style.background = "black"; //Modal Background
 	css[2].cssRules[512].style.backgroundColor = "rgb(0, 0, 0)";
 	css[2].cssRules[2399].style.backgroundColor = "rgb(0, 0, 0)";
 	css[2].cssRules[3241].style.backgroundColor = "rgb(0, 0, 0)";
 	css[2].cssRules[3259].style.backgroundColor = "rgb(0, 0, 0)";
 	css[2].cssRules[512].cssText = `.table-striped > tbody > tr:nth-of-type(2n+1) { "background-color" : "#000000"}`;
+	css[92].cssRules[5].style.background = "black"; //Modal Background
 });
